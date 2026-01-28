879cd1db2a122cdcca6ce8150642032dd51d3165
Mohamad Bader almsaddi alzin <baderalmsaddy@gmail.com>
2026-01-28 05:55:10 +0100

    initial commit added function

--- task-1/book.js
+++ task-1/book.js
@@ -1,3 +1,21 @@
 function isBookApplicable(searchString) {
-  // Your code here
+const GetBookTitle ="The Fundamentals of JavaScript";
+
+    return GetBookTitle.toLowerCase().includes(searchString.trim().toLowerCase());
+
 }
+
+console.log(isBookApplicable("javascript"));
+// Output: true
+
+console.log(isBookApplicable("javascript "));
+// Output: true
+
+console.log(isBookApplicable("python"));
+// Output: false
+
+console.log(isBookApplicable("JavaScript"));
+// Output: true
+
+console.log(isBookApplicable("JAVASCRIPT"));
+// Output: true
